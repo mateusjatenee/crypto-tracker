c0afb41a3f5299d6f161f1ca922044b0a270a285
Mateus Guimarães <mateus.jatene@gmail.com>
2021-12-07 02:06:02 -0300

    remove cash transaction

--- tests/Feature/Models/AccountTest.php
+++ tests/Feature/Models/AccountTest.php
@@ -7,31 +7,6 @@ use App\Transactions\StockTransaction;
 use Database\Factories\AssetFactory;
 use Database\Factories\PositionAggregateFactory;
 
-it('adds a cash transaction to an account', function () {
-    $account = account();
-    $account->addCashTransaction('Some name', 2000);
-
-    expect($account->transactions->first())
-        ->name->toBe('Some name')
-        ->amount->toBe(2000.00)
-        ->quantity->toEqual(1)
-        ->asset_id->toBeNull()
-        ->user->is($account->user)->toBeTrue();
-});
-
-it('calculates the balance of an account', function () {
-    $account = account();
-
-    $account->addCashTransaction('Cash', 3000);
-    $account->addCashTransaction('Cash', 2000);
-
-    expect($account->balance())->toBe(5000.0);
-
-    $account->addCashTransaction('Taxes', -10000);
-
-    expect($account->fresh()->balance())->toBe(-5000.0);
-});
-
 it('it calculates the balance of an asset when it contains variable transactions (stocks)', function () {
     $account = account();
 
